f5ecf0b03183ee95180badbb0d55f2ae6ee86bbe
rajeevsood <erinol@DagnyTaggart.local>
2013-02-24 23:39:08 -0500

    added add migration command

--- lib/db-cm/commands.rb
+++ lib/db-cm/commands.rb
@@ -12,7 +12,6 @@ module Db
       
       desc "init APP_NAME", "Initializes the directory structure"
       def init(app_name)
-
         app_root = File.join(self.destination_root, app_name)
         empty_directory app_root
         self.destination_root = app_root
@@ -20,22 +19,47 @@ module Db
         empty_directory 'environments'
         empty_directory 'bootstrap'
         empty_directory 'migrations' 
-        empty_directory File.join('migrations', 'versionlog')
 
         ['test', 'development', 'production'].each do |env|
           template 'templates/environment.tt', "environments/#{env}.yaml", {:env=>env, :app_name=>app_name}
         end
 
-        create_file File.join('bootstrap', 'custom_01.sql')
-
-
-
-        
+        create_file File.join('bootstrap', '80_custom.sql')
       end
 
-
-
       
+      desc 'add "COMMENT"', "Adds a new migration and associates the comment with it"
+      def add(comment)
+        raise Error, "Currently not in the root of a db-cm created project.  Please go there and try again." if File.directory?('migrations')
+        
+        time_now = Time.now
+        migration_dir = time_now.strftime('%Y%m%d_%H%M%S')
+        migration_base_dir = File.join('migrations', migration_dir)
+        empty_directory migration_base_dir
+
+        create_file File.join(migration_base_dir, '10_ddl.sql')
+        create_file File.join(migration_base_dir, '20_data.sql')
+        create_file File.join(migration_base_dir, '30_constraints.sql')
+        create_file File.join(migration_base_dir, '40_indexes.sql')
+        create_file File.join(migration_base_dir, '50_sequences.sql')
+        create_file File.join(migration_base_dir, '60_triggers.sql')
+        create_file File.join(migration_base_dir, '70_sprocs.sql')
+        create_file File.join(migration_base_dir, '80_custom.sql')
+        create_file File.join(migration_base_dir, '90_grants.sql')
+        create_file File.join(migration_base_dir, 'version.txt'), comment
+
+        undo_base_dir = File.join(migration_base_dir, 'undo')
+        empty_directory undo_base_dir
+        create_file File.join(undo_base_dir, '10_ddl.sql')
+        create_file File.join(undo_base_dir, '20_data.sql')
+        create_file File.join(undo_base_dir, '30_constraints.sql')
+        create_file File.join(undo_base_dir, '40_indexes.sql')
+        create_file File.join(undo_base_dir, '50_sequences.sql')
+        create_file File.join(undo_base_dir, '60_triggers.sql')
+        create_file File.join(undo_base_dir, '70_sprocs.sql')
+        create_file File.join(undo_base_dir, '80_custom.sql')
+        create_file File.join(undo_base_dir, '90_grants.sql')
+      end
 
       
     end
--- spec/bin/db-cm_spec.rb
+++ spec/bin/db-cm_spec.rb
@@ -32,10 +32,6 @@ describe Db::Cm::Commands do
       File.directory?(File.join(@test_root_dir, @test_dir, 'migrations')).should be_true
     end
 
-    it "creates the @test_dir/migrations/versionlog sub-directory" do
-      File.directory?(File.join(@test_root_dir, @test_dir, 'migrations', 'versionlog')).should be_true
-    end
-
     it "creates the @test_dir/environments/test.yaml file" do
       File.exists?(File.join(@test_root_dir, @test_dir, 'environments', 'test.yaml')).should be_true
     end    
@@ -48,12 +44,131 @@ describe Db::Cm::Commands do
       File.exists?(File.join(@test_root_dir, @test_dir, 'environments', 'production.yaml')).should be_true
     end
 
-    it "creates the @test_dir/migrations/versionlog/custom_01.sql" do
-      File.exists?(File.join(@test_root_dir, @test_dir, 'bootstrap', 'custom_01.sql')).should be_true
+    it "creates the @test_dir/migrations/bootstrap/80_custom.sql" do
+      File.exists?(File.join(@test_root_dir, @test_dir, 'bootstrap', '80_custom.sql')).should be_true
+    end
+  end
+
+
+  describe "#add" do
+    before :each do      
+      @sut.init @test_dir
+
+      @comment = 'new version comment'
+      time_now = Time.now
+      @dir_name = time_now.strftime('%Y%m%d_%H%M%S')
+
+      Time.stub!(:now).and_return(time_now)
+      
+      @sut.add @comment
     end
 
+    after :each do
+      FileUtils.remove_dir File.join(@test_root_dir, @test_dir) 
+    end
     
+    it "creates the migration root directory based on today's date" do      
+      File.directory?(File.join(@test_root_dir, @test_dir, 'migrations', @dir_name)).should be_true
+    end
+    
+    it "creates the version.txt based on the comment" do
+      version_filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'version.txt')
+      File.exists?(version_filename).should be_true
+      version_contents = IO.read(version_filename)
+      @comment.should eq version_contents
+    end
+
+    it "creates the 10_ddl.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '10_ddl.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 20_data.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '20_data.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 30_constraints.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '30_constraints.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 40_indexes.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '40_indexes.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 50_sequences.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '50_sequences.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 60_triggers.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '60_triggers.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 70_sprocs.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '70_sprocs.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 80_custom.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '80_custom.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the 90_grants.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, '90_grants.sql')
+      File.exists?(filename).should be_true
+    end
+    
+    it "creates the undo/10_ddl.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '10_ddl.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/20_data.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '20_data.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/30_constraints.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '30_constraints.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/40_indexes.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '40_indexes.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/50_sequences.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '50_sequences.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/60_triggers.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '60_triggers.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/70_sprocs.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '70_sprocs.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/80_custom.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '80_custom.sql')
+      File.exists?(filename).should be_true
+    end
+
+    it "creates the undo/90_grants.sql file" do
+      filename = File.join(@test_root_dir, @test_dir, 'migrations', @dir_name, 'undo', '90_grants.sql')
+      File.exists?(filename).should be_true
+    end
   end
+  
 end
 
 
